f389ed1cc1a5ec6593950f4f45bd27f9c524ae6b
Uires Deivide <uiresdeivide1@gmail.com>
2017-10-05 15:56:49 -0300

    Alterações criação de novo ponteiro FILE

--- txtManiArray.c
+++ txtManiArray.c
@@ -2,21 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+/*
+@auhtor uíres
+
+*/
 int main(){
 	char fraseado [30] = "Shal'na Ilindori";
 
-	FILE *putsFrase;
-	int cont;
+	FILE *putsFrase, *puts2;
+	int cont, cont2;
 	
-	putsFrase = fopen("documentManipu.txt","w");
-	if(putsFrase == NULL){
-		printf("Arquivo indisponível");
-	}else{
+															// puts2 = segundo ponteiro de arquivo 
+	if ((((putsFrase = fopen("manip.txt", "w"))== NULL)) || ((puts2 = fopen("txtTester.txt", "w"))== NULL)) {	
+		printf("Arquivo indisponível! ");
+		exit(1);
+	
+	}else{ // listando com toupper utilizando um for
 		for(cont =0;cont< strlen(fraseado);cont++ ){
 			fputc(toupper(fraseado[cont]), putsFrase);
-		 }
+			fputc(fraseado[cont], puts2);
+			printf("%c", (fraseado[cont]));
+		}
 		fclose(putsFrase);
+	
+		fclose(puts2);
 	}	 
+	
+	
+	
+	
+	
 	
 	return 0;
 }
