3ee0a22b52b7e188f93dbcf67826ca9840dea27f
Uires Deivide <uiresdeivide1@gmail.com>
2017-10-05 15:20:24 -0300

    .

--- txtManipulacao.c
+++ txtManipulacao.c
@@ -6,28 +6,21 @@ int main(){
 	int praiaNome;
 	
 	FILE *fileDB;
-	
-		fileDB = fopen("teste123.txt", "w");
-		fprintf(fileDB, "Listagem -----------\n");
+		
+	fileDB = fopen("teste123.txt", "w");
+	fprintf(fileDB, "Listagem -----------\n");
 	
 	
 	printf("Digite [1] para adicionar dados ou [2] para listagem de dados: ");
 	scanf("%d", &menu);
 	
 	switch(menu){
-		case 1: 		
-			
-			
-			
-			
-			
-			
-			
-			fclose(fileDB);		
-			
+		case 1: 					
+			fclose(fileDB);				
 		break;
 						
-		case 2: printf(";"); break;
+		case 2:
+			if()
 		
 		default: printf("Dado incorreto! tente novamente"); break;			
 	}
